0ab56d258be390b3c4cf68b00a6fad36d64afb94
XIONGPEILIN <s2430069@gl.cc.uec.ac.jp>
2025-05-22 14:42:23 +0900

    test1

--- test/test_inline_image.py
+++ test/test_inline_image.py
@@ -11,7 +11,7 @@ class TestInlineImage(unittest.TestCase):
         layout = {1: Mock(shape=(10, 10), __getitem__=Mock(return_value=0))}
         converter = TranslateConverter(rsrcmgr, layout=layout, lang_in="en", lang_out="zh", service="google")
         interpreter = PDFPageInterpreterEx(rsrcmgr, converter, {})
-        with open('test/file/translate.cli.text.with.figure.pdf', 'rb') as fp:
+        with open('/home/yanai-lab/xiong-p/test/PDFMathTranslate/test-10.pdf', 'rb') as fp:
             page = next(PDFPage.get_pages(fp))
             with patch.object(converter, 'render_inline_image') as mock_render, patch.object(converter, 'receive_layout'):
                 interpreter.process_page(page)
